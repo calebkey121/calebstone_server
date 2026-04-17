be893e4e0f3eee81206616dc46b6b0d8032b3f99
Caleb Key <calebkey121@gmail.com>
2026-04-17 14:15:43 -0500

    get host/port from env

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 # server/run.py
+import os
+
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    host = os.getenv("CALEBSTONE_HOST", "127.0.0.1")
+    port = int(os.getenv("CALEBSTONE_PORT", "5001"))
+    debug = os.getenv("CALEBSTONE_DEBUG", "").lower() in {"1", "true", "yes"}
+    app.run(debug=debug, host=host, port=port)
